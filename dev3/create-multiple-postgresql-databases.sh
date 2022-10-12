3c183df3828118627ec9124da82a51d776aa7c5c
voronov <voronov54@gmail.com>
2022-10-12 14:12:40 +0700

    updated dev configs for posts serivce: case of presharding environment

---
+++ dev3/create-multiple-postgresql-databases.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating database '$database' \n"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$POSTGRES_DB"<<-EOSQL
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+EOSQL
+	echo "  Created database '$database' \n"
+	echo "  Granted all priveleges on '$database' to '$POSTGRES_USER' \n"
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "  Sleep 15 seconds \n"
+    sleep 15
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
